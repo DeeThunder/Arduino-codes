ced1ee683cdb53ad26c7c5d332ec22679c772fdb
Isaac-Great Atanda <119097504+DeeThunder@users.noreply.github.com>
2025-02-27 21:33:44 +0100

    Update arduino_secrets.h

--- IOT WITH ARDUINO CLOUD/DEFENSE_sep19a/arduino_secrets.h
+++ IOT WITH ARDUINO CLOUD/DEFENSE_sep19a/arduino_secrets.h
@@ -1,3 +1,3 @@
 #define SECRET_DEVICE_KEY "ZfvZQRInza5RuI@hDEiC@xoox"
-#define SECRET_OPTIONAL_PASS "@ApiTech"
-#define SECRET_SSID "APITech"
+#define SECRET_OPTIONAL_PASS "password"
+#define SECRET_SSID "Hotspot name"
